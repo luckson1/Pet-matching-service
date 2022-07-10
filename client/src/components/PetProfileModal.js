dd2375b56bce64cbbf5ae65e1fd81d6dacd26735
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-07-10 23:32:09 +0300

    button on tinder card added ontouchstart eventlistener

--- client/src/components/PetProfileModal.js
+++ client/src/components/PetProfileModal.js
@@ -3,7 +3,7 @@ import { PetProfile } from './PetProfile';
 export const PetProfileModal = ({setShowModal, pet}) => {
     return(
         <div className='auth-modal'>
-        <div onClick={() => { setShowModal(false)}} className="close-icon"><i className="bi bi-file-excel"></i></div>
+        <div onClick={() => { setShowModal(false)}} onTouchStart={() => { setShowModal(false) }}className="close-icon"><i className="bi bi-file-excel"></i></div>
         <PetProfile pet={pet}/>
         </div>
     );
