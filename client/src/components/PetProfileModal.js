02769bbacc5e3848f1fd87cff4e21e71e5a4adb7
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-20 22:26:51 +0300

    fix: improve design

--- client/src/components/PetProfileModal.js
+++ client/src/components/PetProfileModal.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { PetProfile } from './PetProfile';
-export const PetProfileModal = ({setShowModal, pet}) => {
-    return(
-        <div className='auth-modal'>
-        <div onClick={() => { setShowModal(false)}} onTouchStart={() => { setShowModal(false) }}className="close-icon "><i className="bi bi-x-circle-fill" style={{color: "red"}}></i></div>
-        <PetProfile pet={pet}/>
+import React from "react";
+import { PetProfile } from "./PetProfile";
+export const PetProfileModal = ({ setShowModal, pet }) => {
+  return (
+    <div className="bg-half-transparent  fixed nav-item top-0 right-0 z-50 fixed-modal">
+      <div className="pet-modal">
+        <div
+          onClick={() => {
+            setShowModal(false);
+          }}
+          className="close-icon "
+        >
+          <i className="bi bi-x-circle-fill" style={{ color: "red" }}></i>
         </div>
-    );
+        <PetProfile pet={pet} />
+      </div>
+    </div>
+  );
 };
