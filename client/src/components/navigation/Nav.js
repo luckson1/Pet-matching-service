214b2424256db591eb42017382ba9ebf14027b11
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-20 22:26:03 +0300

    fix: improve navigation

--- client/src/components/navigation/Nav.js
+++ client/src/components/navigation/Nav.js
@@ -1,22 +1,112 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { logout } from '../../redux/usersSlices';
-import logo from "../images/logo.png"
-export const Nav = ({ authToken, setShowModal, showModal, setIsSignUp,isDashboard, isFavPets }) => {
-    const dispatch= useDispatch()
-    const navigate= useNavigate()
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { MdClose, MdMenu } from "react-icons/md";
+import { logout } from "../../redux/usersSlices";
+import logo from "../images/logo.png";
+export const Nav = ({ authToken, setShowModal, showModal, setIsSignUp}) => {
 
-    return (
-        <nav>
-           <div className="logo-container">
-                <img className='logo' src={logo} alt="logo" onClick={() => { navigate("/")}} />
-            </div>
-            {authToken && isFavPets? <button className='nav-button' onClick={() => { navigate("/favourite-pets")}} disabled={showModal}><i class="bi bi-eye"></i> Favourites</button>
-            : null}
-              {authToken && isDashboard? <button className='nav-btn' onClick={() => { navigate("/dashboard") }} >Dashboard</button>
-            : null}
-            {!authToken? <button className='nav-btn' onClick={() => { setShowModal(true); setIsSignUp(false) }} disabled={showModal}>Login</button>
-            : <button className='logout-button' onClick={() => { dispatch(logout())}} disabled={showModal}>Logout</button>}
-        </nav>);
+  const [isOpenMenu, setIsOpenMenu] = useState(false); 
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userLogin = useSelector((state) => state?.users?.userAuth);
+
+
+  return (
+    <nav
+      className="fixed w-11/12 z-30 top-0  dark:text-slate-10 mx-10 "
+    >
+      <div className="flex justify-center items-center h-12 w-12 mt-3 ml-2">
+        <img
+          className="logo"
+          src={logo}
+          alt="logo"
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+      </div>
+      <div className="w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2 bg-white  dark:bg-[#484B52]">
+        <div className="pl-4 flex items-center"></div>
+        <div className="block lg:hidden pr-4">
+          <button
+            onClick={() => setIsOpenMenu(!isOpenMenu)}
+            id="nav-toggle"
+            className="flex items-center p-1 text-gray-900 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+          >
+            {isOpenMenu ? <MdClose /> : <MdMenu />}
+          </button>
+        </div>
+        <div
+          className={`w-full flex-grow md:flex md:items-center md:w-auto ${
+            isOpenMenu ? "" : "hidden"
+          } mt-2 md:mt-0 bg-white  dark:bg-[#484B52] dark:text-slate-100 text-black p-4 md:p-0 z-10" `}
+        >
+          <ul className="list-reset md:flex  flex-1 items-center ">
+            <li className=" mr-96 md:mr-3  ">
+              <Link
+                onClick={() => setIsOpenMenu(!isOpenMenu)}
+                className="inline-block  py-2 px-4 text-black dark:text-slate-100 font-bold no-underline "
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+           
+            {userLogin &&  <> <li className="mr-96 md:mr-3 ">
+              <Link
+                onClick={() => setIsOpenMenu(!isOpenMenu)}
+                className="inline-block text-black dark:text-slate-100 no-underline hover:text-gray-900 hover:text-underline py-2 px-4"
+                to="/dashboard"
+              >
+               Dashboard
+              </Link>
+            </li>
+            <li className="mr-96 md:mr-3 ">
+              <Link
+                onClick={() => setIsOpenMenu(!isOpenMenu)}
+                className="inline-block text-black dark:text-slate-100 no-underline hover:text-gray-900 hover:text-underline py-2 px-4"
+                to="/favourite-pets">
+                Favourites
+              </Link>
+            </li>
+           
+              <li className="mr-96 md:mr-3 ">
+                <Link
+                  // onClick={() => {
+                  //   setIsOpenMenu(!isOpenMenu);
+                  //   setActiveMenu(true);
+                  //   setShowNavBar(true);
+                  // }}
+                  className="inline-block text-black dark:text-slate-100 no-underline hover:text-gray-900 hover:text-underline py-2 px-4"
+                  to="/admin-dashboard"
+                >
+                  Admin
+                </Link>
+              </li>
+              </>}
+           
+          </ul>
+          <button        
+            className="mx-auto mr-96 lg:mx-0 hover:underline bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-900 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+
+            onClick={
+              userLogin
+                ? () => dispatch(logout())
+                : () => {            //                 //     
+                    setIsOpenMenu(false);
+                    setShowModal(true);
+                    setIsSignUp(false)
+                  }
+            }
+          >
+            {userLogin ? "Logout" : "Login"}
+          </button>
+        </div>
+      </div>
+      <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+    </nav>
+  );
 };
