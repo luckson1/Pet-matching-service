a5419c0e44a7060e7c8823902e15471c1be73ed9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-20 22:27:41 +0300

    fix: format and improve design

--- client/src/components/Authmodal.js
+++ client/src/components/Authmodal.js
@@ -1,136 +1,163 @@
-import { useFormik } from "formik"
-import * as Yup from 'yup'
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUserAction, registerUserAction } from '../redux/usersSlices';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUserAction, registerUserAction } from "../redux/usersSlices";
 import DisabledButton from "./DisabledButton";
 import { useState } from "react";
 
-// use yup to handle errors 
+// use yup to handle errors
 const SignInErrorSchema = Yup.object().shape({
-    firstName: Yup.string()
-        .min(2, 'First name Too Short!')
-        .max(50, 'First name Too Long!')
-        .required('First name Required'),
-    lastName: Yup.string()
-        .min(2, 'Last name Too Short!')
-        .max(50, 'Last name Too Long!')
-        .required('Last name Required'),
-    email: Yup.string().email('Invalid email').required('Email Required'),
-    password: Yup.string()
-    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-        ,'password must contain 6 or more characters with at least one of each: uppercase, lowercase, number and special character'
-      )
-      
-        .required('Password Required'),
-        
+  firstName: Yup.string()
+    .min(2, "First name Too Short!")
+    .max(50, "First name Too Long!")
+    .required("First name Required"),
+  lastName: Yup.string()
+    .min(2, "Last name Too Short!")
+    .max(50, "Last name Too Long!")
+    .required("Last name Required"),
+  email: Yup.string().email("Invalid email").required("Email Required"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "password must contain 6 or more characters with at least one of each: uppercase, lowercase, number and special character"
+    )
+
+    .required("Password Required"),
 });
 const LoginErrorSchema = Yup.object().shape({
-   
-    email: Yup.string().email('Invalid email').required('Email Required'),
-    password: Yup.string()
-      .required('Password Required'),
-        
+  email: Yup.string().email("Invalid email").required("Email Required"),
+  password: Yup.string().required("Password Required"),
 });
 
-export const Authmodal = ({ setShowModal, isSignUp, setIsSignUp}) => {
-const [reveal, setReveal]=useState(false)
-    // dispatch
-const dispatch=useDispatch()
+export const Authmodal = ({ setShowModal, isSignUp, setIsSignUp }) => {
+  const [reveal, setReveal] = useState(false);
+  // dispatch
+  const dispatch = useDispatch();
 
-// get data from store
+  // get data from store
 
-const user = useSelector((state) => {
-    return state?.users
-})
-const { userLoading, userServerErr, userAppErr}=user;
-    // use formik hook to handle form state 
-   const formik = useFormik({
-       initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            gender: '',
-            petPreference: "",
-            children: "",
-            petOwned: "",
-            garden: "",
-            active: "",
-            about: "",
-            image: ""
+  const user = useSelector((state) => {
+    return state?.users;
+  });
+  const { userLoading, userServerErr, userAppErr } = user;
+  // use formik hook to handle form state
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      gender: "",
+      petPreference: "",
+      children: "",
+      petOwned: "",
+      garden: "",
+      active: "",
+      about: "",
+      image: "",
+    },
 
-        },
-    
-        validationSchema: isSignUp? SignInErrorSchema: LoginErrorSchema,
-        onSubmit: values => {
-            isSignUp? dispatch(registerUserAction(values)):  dispatch(loginUserAction(values))
-        },
-    })
-    
-    return (<div className='auth-modal'>
-        <div onClick={() => { setShowModal(false); setIsSignUp(true) }} className="close-icon"><i className="bi bi-file-excel"></i></div>
-        <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
-       { isSignUp && <p> By clicking Sign Up you are in agreement with our terms. Learn more from our Privacy Policy Page</p>}
-       {/* Errors */}
-       {userAppErr || userServerErr ? (
-                <div className="form-validation" role="alert">
-                  {userServerErr} {userAppErr}
-                </div>
-              ) : null}
-        <form onSubmit={formik.handleSubmit} >
+    validationSchema: isSignUp ? SignInErrorSchema : LoginErrorSchema,
+    onSubmit: (values) => {
+      isSignUp
+        ? dispatch(registerUserAction(values))
+        : dispatch(loginUserAction(values));
+    },
+  });
 
-            { isSignUp && <input
-                value={formik.values.firstName}
-                onChange={formik.handleChange("firstName")}
-                onBlur={formik.handleBlur("firstName")}
-                type="text"
-                placeholder="First Name"
-            />}
-            {/* Err */}
-            <div className="form-validation">
-                {formik.touched.firstName && formik.errors.firstName}
-            </div>
-            {isSignUp && <input
-                value={formik.values.lastName}
-                onChange={formik.handleChange("lastName")}
-                onBlur={formik.handleBlur("lastName")}
-                type="text"
-                placeholder="Last Name"
-            />}
-            {/* Err */}
-            <div className="form-validation">
-                {formik.touched.lastName && formik.errors.lastName}
-            </div>
-            <input
-                value={formik.values.email}
-                onChange={formik.handleChange("email")}
-                onBlur={formik.handleBlur("email")}
-                type="email"
-                placeholder="E-mail address"
-            />
-            {/* Err */}
-            <div className="form-validation">
-                {formik.touched.email && formik.errors.email}
-            </div>
-            <input
-                value={formik.values.password}
-                onChange={formik.handleChange("password")}
-                onBlur={formik.handleBlur("password")}
-                type={reveal?"text":"password"}
-                placeholder="Password"
-            />
-                <div className="toggle-icon"><i className={reveal? "bi bi-eye-slash ": "bi bi-eye "  }id="togglePassword" onClick={()=> setReveal(!reveal)}></i></div> 
-            {/* Err */}
-            <div className="form-validation">
-                {formik.touched.password && formik.errors.password}
-            </div>
-        {userLoading? <DisabledButton /> :     <button
-                type="submit"
-                className="secondary-button"
-            >
-                {isSignUp? "Sign Up": "Login"}
-            </button>}
-        </form>
-
-    </div>);
+  return (
+    <div className="auth-modal bg-gradient-to-b from-sky-300 via-fuchsia-300 to-cyan-300">
+      <div
+        onClick={() => {
+          setShowModal(false);
+          setIsSignUp(true);
+        }}
+        className="close-icon"
+      >
+        <i className="bi bi-file-excel"></i>
+      </div>
+      <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      {isSignUp && (
+        <p>
+          {" "}
+          By clicking Sign Up you are in agreement with our terms. Learn more
+          from our Privacy Policy Page
+        </p>
+      )}
+      {/* Errors */}
+      {userAppErr || userServerErr ? (
+        <div className="form-validation" role="alert">
+          {userServerErr} {userAppErr}
+        </div>
+      ) : null}
+      <form onSubmit={formik.handleSubmit}>
+        {isSignUp && (
+          <input
+            value={formik.values.firstName}
+            className="rounded"
+            onChange={formik.handleChange("firstName")}
+            onBlur={formik.handleBlur("firstName")}
+            type="text"
+            placeholder="First Name"
+          />
+        )}
+        {/* Err */}
+        <div className="form-validation">
+          {formik.touched.firstName && formik.errors.firstName}
+        </div>
+        {isSignUp && (
+          <input
+            className="rounded"
+            value={formik.values.lastName}
+            onChange={formik.handleChange("lastName")}
+            onBlur={formik.handleBlur("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+        )}
+        {/* Err */}
+        <div className="form-validation">
+          {formik.touched.lastName && formik.errors.lastName}
+        </div>
+        <input
+          value={formik.values.email}
+          className="rounded"
+          onChange={formik.handleChange("email")}
+          onBlur={formik.handleBlur("email")}
+          type="email"
+          placeholder="E-mail address"
+        />
+        {/* Err */}
+        <div className="form-validation">
+          {formik.touched.email && formik.errors.email}
+        </div>
+        <input
+          value={formik.values.password}
+          className="rounded"
+          onChange={formik.handleChange("password")}
+          onBlur={formik.handleBlur("password")}
+          type={reveal ? "text" : "password"}
+          placeholder="Password"
+        />
+        <div className="toggle-icon">
+          <i
+            className={reveal ? "bi bi-eye-slash " : "bi bi-eye "}
+            id="togglePassword"
+            onClick={() => setReveal(!reveal)}
+          ></i>
+        </div>
+        {/* Err */}
+        <div className="form-validation">
+          {formik.touched.password && formik.errors.password}
+        </div>
+        {userLoading ? (
+          <DisabledButton />
+        ) : (
+          <button type="submit" className="secondary-button">
+            {isSignUp ? "Sign Up" : "Login"}
+          </button>
+        )}
+      </form>
+    </div>
+  );
 };
