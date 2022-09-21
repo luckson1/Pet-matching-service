e5f4e8214512a66c2351b9646459b6489910e3b0
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-21 16:25:26 +0300

    fix: backend ap1

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,2 +1,1 @@
-// export const BaseURL=  "https://adopt-get-pet.herokuapp.com/api"
-export const BaseURL=  "http://localhost:4000/api"
+export const BaseURL=  "https://adopt-get-pet.herokuapp.com/api"
