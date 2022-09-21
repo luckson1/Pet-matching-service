799f264a55fec468b959ee3323664c2a0fa89218
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-21 15:37:06 +0300

    fiix: change props format

--- client/src/components/AdminDashboardCard.js
+++ client/src/components/AdminDashboardCard.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router';
+import React from "react";
+import { useNavigate } from "react-router";
 
-export default function AdminDashboardCard({src, title, redirection, buttonText}) {
-    const navigate = useNavigate();
+export default function AdminDashboardCard({
+  src,
+  title,
+  redirectionUrl,
+  buttonText,
+  state,
+}) {
+  const navigate = useNavigate();
   return (
     <div className="h-64 w-64 bg-indigo-100 rounded-lg shadow-lg flex flex-col justify-between items-center py-7 mb-7">
-    <img src={src} alt="placeHolder" className="h-12 w-12 rounded-full"/>
-  <p className="w-56 text-left">{title}</p>
-  <button
-    className=" bg-gradient-to-r from-indigo-500 via-violet-500 to-pink-500 text-gray-900 font-bold rounded-lg w-24 h-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-    onClick={() => navigate(redirection)}
-  >
-    {buttonText}
-  </button>
-</div>
-  )
+      <img src={src} alt="placeHolder" className="h-12 w-12 rounded-full" />
+      <p className="w-56 text-left">{title}</p>
+      <button
+        className=" bg-gradient-to-r from-indigo-500 via-violet-500 to-pink-500 text-gray-900 font-bold rounded-lg w-24 h-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+        onClick={() => navigate(redirectionUrl, (state = { state }))}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
 }
