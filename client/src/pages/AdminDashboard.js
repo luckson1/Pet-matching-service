a5f57cc85d8dac1bd26e0d510194788053625eb9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-21 15:42:54 +0300

    fix: produce ids needed to filter adopters and pets

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -17,52 +17,79 @@ export const AdminDashboard = () => {
   const petsData = useSelector((state) => state?.pets?.allPets?.pets);
 
   const usersData = useSelector((state) => state?.users?.allUsers?.users);
-
+  //get ids of Dogs
   const dogs = petsData?.filter((pet) => pet?.petType === "dogs");
+  let dogIds = [];
+  dogs?.map((dog) => dogIds.push(dog?._id));
+
+  // get ids of Cats
   const cats = petsData?.filter((pet) => pet?.petType === "cats");
+  let catIds = [];
+  cats?.map((cat) => catIds.push(cat?._id));
+
+  //get ids of Dogs Adopters
   const dogAdopters = usersData?.filter(
     (user) => user?.petPreference === "dogs"
   );
+  let dogAdopterIds = [];
+  dogAdopters?.map((dogAdopter) => dogAdopterIds.push(dogAdopter?._id));
+
+  //filter ids of Cat Adopters
+
   const catAdopters = usersData?.filter(
     (user) => user?.petPreference === "cats"
   );
+
+  let catAdopterIds = [];
+  catAdopters?.map((catAdopter) => catAdopterIds.push(catAdopter?._id));
+
+  // filter ids of those with no prefrence
   const noPreferenceAdopters = usersData?.filter(
     (user) => user?.petPreference === "any"
   );
-  console.log(catAdopters);
+
+  let noPreferenceAdopterIds = [];
+  noPreferenceAdopters?.map((adopter) =>
+    noPreferenceAdopterIds.push(adopter?._id)
+  );
   return (
     <section>
       <Nav />
-      <div className="mt-24 mb-5 mx-20 flex flex-row flex-wrap justify-center md:justify-between gap-1 ">
+      <div className="mt-24 mb-5 mx-20 flex flex-row flex-wrap justify-center  gap-5 ">
         <AdminDashboardCard
           src={Adopter}
           title={`${dogAdopters?.length} Adopters Looking for a Dog`}
           buttonText={"View"}
-          redirection={"/adopters"}
+          redirectionUrl={"/adopters"}
+          state={dogAdopterIds}
         />
         <AdminDashboardCard
           src={Adopter}
           title={`${catAdopters?.length} Adopters Looking for a Cat`}
           buttonText={"View"}
-          redirection={"/adopters"}
+          redirectionUrl={"/adopters"}
+          state={catAdopterIds}
         />
         <AdminDashboardCard
           src={Adopter}
           title={`${noPreferenceAdopters?.length} Adopters Without Particular Preference`}
           buttonText={"View"}
-          redirection={"/adopters"}
+          redirectionUrl={"/adopters"}
+          state={noPreferenceAdopterIds}
         />
         <AdminDashboardCard
           src={Dog}
           title={`${dogs?.length} Dogs Available for Adoption`}
           buttonText={"View"}
-          redirection={"/all-Pets"}
+          redirectionUrl={"/all-Pets"}
+          state={dogIds}
         />
         <AdminDashboardCard
           src={Cat}
           title={`${cats?.length} Cats Available for Adoption`}
           buttonText={"View"}
-          redirection={"/all-Pets"}
+          redirectionUrl={"/all-Pets"}
+          state={catIds}
         />
       </div>
     </section>
