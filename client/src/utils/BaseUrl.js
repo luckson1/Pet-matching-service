1bf535985cf0d02685b37fa9be9f6650b8b4781c
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-21 16:05:14 +0300

    format

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BaseURL=  "https://adopt-get-pet.herokuapp.com/api"
+// export const BaseURL=  "https://adopt-get-pet.herokuapp.com/api"
+export const BaseURL=  "http://localhost:4000/api"
