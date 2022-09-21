d4b2f3a872013763843dd850d6691d35d2a6eed0
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-09-21 15:41:18 +0300

    fix: introduce filtering of adopters

--- client/src/pages/AdoptersData.js
+++ client/src/pages/AdoptersData.js
@@ -1,30 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
 import { Nav } from "../components/navigation/Nav";
 import UsersList from "../components/UsersList";
 import { fetchAllUsersAction } from "../redux/usersSlices";
 
-
 function AdoptersData() {
- 
-const [adopters, setAdopters]= useState([])
+  const [adopters, setAdopters] = useState([]);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const selectedAdoptersIds = location?.state;
   useEffect(() => {
     dispatch(fetchAllUsersAction());
   }, []);
 
-  const usersData = useSelector((state) => state?.users?.allUsers?.users);
+  const adoptersData = useSelector((state) => state?.users?.allUsers?.users);
 
- 
   useEffect(() => {
-    if (typeof usersData !== "undefined") setAdopters(usersData);
-  }, [usersData]);
+    if (typeof adoptersData !== "undefined" && selectedAdoptersIds === null)
+      setAdopters(adoptersData);
+    if (typeof adoptersData !== "undefined" && selectedAdoptersIds !== null) {
+      // filter adopters based on pereference type
+      let filteredAdopters = adoptersData?.filter((adopter) =>
+        selectedAdoptersIds?.includes(adopter?._id)
+      );
+
+      setAdopters(filteredAdopters);
+    }
+  }, [adoptersData]);
 
   return (
     <div>
-     <Nav />
+      <Nav />
       <div className="mt-28">
-        <UsersList users={adopters}/>
+        <UsersList users={adopters} />
       </div>
     </div>
   );
