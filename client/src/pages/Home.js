3c85498922dc1a618fa6858a74cf081d041285ac
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-10-02 21:52:29 +0300

    chore: rm unused vars and format

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,68 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { Authmodal } from '../components/Authmodal';
-import { Nav } from '../components/navigation/Nav';
-import { logout } from '../redux/usersSlices';
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { Authmodal } from "../components/Authmodal";
+import { Nav } from "../components/navigation/Nav";
+import { logout } from "../redux/usersSlices";
 
 export const Home = () => {
-const [showModal, setShowModal]=useState(false)
-const [isSignUp, setIsSignUp]=useState(true)
-//show dashboard button
-const isDashboard=true
+  const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
 
-// get data from store
+  // get data from store
 
-const user = useSelector((state) => {
-    return state?.users
-})
-const {isLoggedIn, isRegistered,userAuth}=user;
+  const user = useSelector((state) => {
+    return state?.users;
+  });
+  const { isLoggedIn, isRegistered, userAuth } = user;
 
+  // force navigation once an action is performed
+  const navigate = useNavigate();
 
-// force navigation once an action is performed
-const navigate=useNavigate();
-
-useEffect(() => {
+  useEffect(() => {
     if (isRegistered) {
-       
-      navigate('/onboarding');
+      navigate("/onboarding");
       window.location.reload();
-    
-
     }
-  }, [isRegistered, navigate])
+  }, [isRegistered, navigate]);
 
+  const authToken = userAuth;
 
+  useEffect(() => {
+    if (isLoggedIn) {
+      return navigate("/dashboard");
+    }
+  }, [isLoggedIn, navigate, userAuth]);
 
- 
-
-    const authToken = userAuth;
-    
-
-    useEffect(()=> {
-        if (isLoggedIn){
-            return navigate('/dashboard')
-        } 
-        }, [isLoggedIn, navigate, userAuth])
-       
-            
-
-   
-
-    const dispatch= useDispatch()
-    return (
-        <div className='overlay'>
-            <Nav setShowModal={setShowModal} showModal={showModal} setIsSignUp={setIsSignUp} isDashboard={isDashboard} />
-            <div>
-                <div className='home'>
-                    <h1 className="primary-title">Get A Pet</h1>
-                    <button className=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-900 font-bold rounded-full mb-4  py-4 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-                    onClick={()=> {authToken? dispatch(logout()): setShowModal(true)}}>
-                        {authToken ? "Sign out" : "create Account"}
-                    </button>
-                    {showModal && <Authmodal setShowModal={setShowModal} isSignUp={isSignUp} setIsSignUp={setIsSignUp}/>}
-                </div>
-            </div>
-        </ div>);
+  const dispatch = useDispatch();
+  return (
+    <div className="overlay">
+      <Nav
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
+      <>
+        <div className="home">
+          <h1 className="primary-title">Get A Pet</h1>
+          <button
+            className=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-900 font-bold rounded-full mb-4  py-4 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            onClick={() => {
+              authToken ? dispatch(logout()) : setShowModal(true);
+            }}
+          >
+            {authToken ? "Sign out" : "create Account"}
+          </button>
+          {showModal && (
+            <Authmodal
+              setShowModal={setShowModal}
+              isSignUp={isSignUp}
+              setIsSignUp={setIsSignUp}
+            />
+          )}
+        </div>
+      </>
+    </div>
+  );
 };
