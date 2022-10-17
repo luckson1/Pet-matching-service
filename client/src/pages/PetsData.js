862361c64e081d702b4d89c4576b4760ced0b2cc
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-10-17 23:47:40 +0300

    fix: spelling in hook and add dependecies

--- client/src/pages/PetsData.js
+++ client/src/pages/PetsData.js
@@ -7,7 +7,7 @@ import PetsList from "../components/PetsList";
 import { fetchAllpetsAction } from "../redux/petsSlices";
 
 function PetsData() {
-  const [pets, setpets] = useState([]);
+  const [pets, setPets] = useState([]);
   const dispatch = useDispatch();
   const location = useLocation();
   const selectedPetsIds = location?.state;
@@ -19,16 +19,16 @@ function PetsData() {
 
   useEffect(() => {
     if (typeof petsData !== "undefined" && selectedPetsIds === null)
-      setpets(petsData);
+      setPets(petsData);
     if (typeof petsData !== "undefined" && selectedPetsIds !== null) {
       // filter pets selected by a user as their favourite or based on type of pet
       let filteredPets = petsData?.filter((pet) =>
         selectedPetsIds?.includes(pet?._id)
       );
      
-      setpets(filteredPets);
+      setPets(filteredPets);
     }
-  }, [petsData]);
+  }, [petsData, selectedPetsIds]);
 
   return (
     <div>
@@ -39,7 +39,7 @@ function PetsData() {
         <Link to="/admin-dashboard">Back to Admin</Link>
         </div>
         
-        <PetsList pets={pets} setPets={setpets} />
+        <PetsList pets={pets} setPets={setPets} />
       </div>
     </div>
   );
