aa44591a6f77b5f0da4ff19df203b8e8c584bcbe
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-11-11 00:25:33 +0300

    fix: make better

---
+++ client/src/pages/RegistrationSuccess.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import success from "./images/success.svg";
+const RegistrationSuccess = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "20px",
+      }}
+    >
+      <h1 className="text-danger">Account Successfully Created</h1>
+      <div className="mb-10 underline text-blue-500 hover:text-blue-900 flex flex-row justify-end mx-44">
+        <Link to="/">Login to Continue</Link>
+        </div>
+      <img alt="success" className=" img-fluid m-3" src={success} />
+    </div>
+  );
+};
+
+export default  RegistrationSuccess;
