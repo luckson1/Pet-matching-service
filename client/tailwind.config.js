a1b3afb97fa79a55590ee308d4ca7293126807fc
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-06 03:27:26 +0300

    fix: add daisy ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#46518C",
+         " primary-content": "#FFFFFF",
+          secondary: "#D9A648",
+          accent: "#F2D49B",
+          neutral: "#3d4451",
+          "base-100": "#fff",
+        },
+      },
+      "dark",
+   
+    ],
   },
-  plugins: [],
-}
+
+};
